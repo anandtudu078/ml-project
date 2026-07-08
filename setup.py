151eb039c5935b5a6d7fd2f6d2c48d4222f9a692
Anand Tudu <anandtudu078@gmail.com>
2026-07-09 03:23:42 +0530

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Anand',
+    author_email='anandtudu0303@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn'],
+
+
+
+
+)
