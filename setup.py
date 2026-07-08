26468c9e23f4d9107ca0060b6d8b323764e25cf2
Anand Tudu <anandtudu078@gmail.com>
2026-07-09 03:56:31 +0530

    Setup.py and requirements

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-from setuptools import find_packages,setup
-from typing import list
+from setuptools import find_packages, setup
+from typing import List
 
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns the list of requirements.
+    """
 
-    '''
-    this function return the list of requirements",
-    '''
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
 
-    requirements=[]
-    with open(file_path)as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ")for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Anand',
-    author_email='anandtudu0303@gmail.com',
+    name="mlproject",
+    version="0.0.1",
+    author="Anand",
+    author_email="anandtudu0303@gmail.com",
     packages=find_packages(),
-    install_requires= get_requirements('requirements.txt')
-
-
-
+    install_requires=get_requirements("requirements.txt"),
 )
