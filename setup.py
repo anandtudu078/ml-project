4f0ba00932389fc7b27972c36d2137aa3fdae371
Anand Tudu <anandtudu078@gmail.com>
2026-07-09 03:37:41 +0530

    updated the files

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages,setup
+from typing import list
+
+
+def get_requirements(file_path:str)->list[str]:
+
+    '''
+    this function return the list of requirements",
+    '''
+
+    requirements=[]
+    with open(file_path)as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ")for req in requirements]
 
 
 setup(
@@ -7,8 +20,7 @@ setup(
     author='Anand',
     author_email='anandtudu0303@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn'],
-
+    install_requires= get_requirements('requirements.txt')
 
 
 
